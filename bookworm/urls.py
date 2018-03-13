5f61762c0c73f197ce7d8bffeaceddf57e365be4
Vladas Maldzius <2262803m@student.gla.ac.uk>
2018-03-13 19:11:51 +0000

    Migrate

--- bookworm/urls.py
+++ bookworm/urls.py
@@ -1,9 +1,6 @@
 from django.conf.urls import url
-from django.contrib import admin
-from django.conf.urls import include
 from bookworm import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^admin/', admin.site.urls),
 ]

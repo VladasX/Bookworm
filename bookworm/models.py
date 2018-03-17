f0c7a03b25fb5ecd0342e916f6184711511da198
HSSFOX <35367324+2260181h@users.noreply.github.com>
2018-03-17 04:10:03 +0000

    updated book and review to the model

--- bookworm/models.py
+++ bookworm/models.py
@@ -3,9 +3,31 @@ from django.contrib.auth.models import User
 
 #Model for user profile.
 class UserProfile(models.Model):
-	user = models.OneToOneField(User)
+	user = models.OneToOneField(User, primary_key=True)
 	bio = models.CharField(max_length=2000, blank=True)
 	picture = models.ImageField(upload_to='profile_images', blank=True)
 
 	def __str__(self):
 		return self.user.username
+
+
+class Book(models.Model):
+	title = models.CharField(max_length=256),
+	author = models.CharField(max_length=128),
+	publisher = models.CharField(max_length=2000),
+	publishedDate = models.CharField(max_length=2000),
+	description = models.TextField(max_length=4096, blank=True),
+	isbn = models.Integer(ForeignKey=True),
+	averageRating = models.FloatField(max_length=2000),
+
+	def __str__(self):
+		return self.title, self.author, self.publisher， self.publishedDate, self.description, self.isbn, self.averageRating
+
+
+class Review(models.Model):
+	comment = models.ForeignKey(Book),
+	date = models.dateTimeField(auto_now=False, auto_add=False),
+	user = models.ForeignKey(UserProfile),
+	text = models.TextField(max_length=2000),
+
+#class BookList(models.Model):

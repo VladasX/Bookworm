2b5847d9ed64997ea8f17a9627be71d19b44fce1
Jaffer N <2194838N@student.gla.ac.uk>
2018-03-17 18:06:34 +0000

    Fixed some of the problems in the new models that were throwing errors.

--- bookworm/models.py
+++ bookworm/models.py
@@ -10,24 +10,25 @@ class UserProfile(models.Model):
 	def __str__(self):
 		return self.user.username
 
-
+#Model for books stored.
 class Book(models.Model):
 	title = models.CharField(max_length=256),
 	author = models.CharField(max_length=128),
-	publisher = models.CharField(max_length=2000),
-	publishedDate = models.CharField(max_length=2000),
+	publisher = models.CharField(max_length=256),
+	publishedDate = models.CharField(max_length=256),
+	thumbnail = models.CharField(max_length=2000)
 	description = models.TextField(max_length=4096, blank=True),
-	isbn = models.Integer(ForeignKey=True),
-	averageRating = models.FloatField(max_length=2000),
+	linkid = models.CharField(max_length=256), #Uses the Google page ID instead of the ISBN since not all results via the API seem to return an ISBN.
+	averageRating = models.FloatField(max_length=5)
 
 	def __str__(self):
-		return self.title, self.author, self.publisher， self.publishedDate, self.description, self.isbn, self.averageRating
+		return self.title, self.author, self.publisher, self.publishedDate, self.description, self.linkid, self.averageRating
 
-
+#Model for reviews stored.
 class Review(models.Model):
-	comment = models.ForeignKey(Book),
-	date = models.dateTimeField(auto_now=False, auto_add=False),
+	book = models.ForeignKey(Book),
 	user = models.ForeignKey(UserProfile),
-	text = models.TextField(max_length=2000),
+	date = models.DateField(auto_now_add=True),
+	text = models.TextField(max_length=4096)
 
 #class BookList(models.Model):

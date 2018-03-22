1e5c6fcbe235b48acb7b0cd080566bb43e845602
Jaffer N <2194838N@student.gla.ac.uk>
2018-03-22 17:09:30 +0000

    Hidden the search bar if on the search page.

--- static/js/bookworm-jquery.js
+++ static/js/bookworm-jquery.js
@@ -1,9 +1,24 @@
 // JQuery code to be added in here.
 
-// Used to pass book's title and description to a popup window
+// Used to pass book's title and description to a popup window.
 $('#book-modal').on('show.bs.modal', function (event) {
   var bookTitle = $(event.relatedTarget).data('title');
   var bookDesc = $(event.relatedTarget).data('desc');
   $(this).find(".modal-header").text(bookTitle);
   $(this).find(".modal-body").text(bookDesc);
 });
+
+// Used to toggle what navbar item is active.
+$(document).ready(function() {
+  $('li.active').removeClass('active');
+  $('a[href="' + location.pathname + '"]').addClass('active'); 
+});
+
+// Used to hide the search bar if on the search page.
+$(function(){
+      if (window.location.pathname == "/search/") {
+            $('#navsearchbar').hide();
+      } else {
+            $('#navsearchbar').show();
+      }
+ });
